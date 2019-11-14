045d5e66800a168e02045a9ef44dca9ff41476ea
Lincoln <lincolnkushwah009@gmail.com>
2019-11-14 16:29:28 +0530

    profile

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -9,9 +9,13 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
+
+
     return Container(
+      
       color: Colors.white,
       child: Column(children: [
+        
         Padding(
           padding: const EdgeInsets.fromLTRB(0, 30, 200, 0),
           child: Text(
@@ -46,27 +50,137 @@ class _ProfileState extends State<Profile> {
 
                 )
             ),
+
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 20, 220, 0),
+          child: Container(
+            child: Text("Activity goals"),
+          ),
+        ),
+
+        Divider(
+          height: 40
+        ),
+        SizedBox(height: 10),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Row(
+            children: <Widget>[
+
+              const Expanded(
+
+                child: TextField(
+                  decoration: InputDecoration(
+
+                      labelText: 'Move Minutes',
+                      border: OutlineInputBorder()),
+                  keyboardType: TextInputType.number,
+
+                ),
+
+              ),
+              SizedBox(width: 10,),
+
+              const Expanded(
+
+                child: TextField(
+                  decoration: InputDecoration(
+
+                      labelText: 'Heart Points',
+                      border: OutlineInputBorder()),
+                  keyboardType: TextInputType.number,
+
+                ),
+
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(0, 30, 230, 0),
+          child: Container(
+            child: Text("About You"),
           ),
         ),
         Divider(
-          height: 40,
+            height: 40
         ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Row(
+            children: <Widget>[
+
+              const Expanded(
+
+                  child: TextField(
+                    decoration: InputDecoration(
+
+                        labelText: 'Gender',
+                        border: OutlineInputBorder()),
+                    keyboardType: TextInputType.number,
+
+                  ),
+
+              ),
+              SizedBox(width: 10,),
+
+              const Expanded(
+
+                child: TextField(
+                  decoration: InputDecoration(
+
+                      labelText: 'Birthday',
+                      border: OutlineInputBorder()),
+                  keyboardType: TextInputType.number,
+
+                ),
+
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+          child: Row(
+            children: <Widget>[
+
+              const Expanded(
+
+                child: TextField(
+                  decoration: InputDecoration(
+
+                      labelText: 'Weight',
+                      border: OutlineInputBorder()),
+                  keyboardType: TextInputType.number,
+
+                ),
+
+              ),
+              SizedBox(width: 10,),
+
+              const Expanded(
+
+                child: TextField(
+                  decoration: InputDecoration(
+
+                      labelText: 'Height',
+                      border: OutlineInputBorder()),
+                  keyboardType: TextInputType.number,
+
+                ),
+
+              )
+            ],
+          ),
+        ),
+
 
       ]),
 
     );
   }
 }
-/*
- child: TextField(
-        decoration: InputDecoration(
-            labelText: 'Text',
-            hintText: 'Insert some text',
-            border: OutlineInputBorder()),
-        onSubmitted: (text) {
-          setState(() {
 
-          });
-        },
-      ),
- */
+
